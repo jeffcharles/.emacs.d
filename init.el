9bd478472b87f35b1bb8a6568722e0a520b1f66a
Jeffrey Charles <jeffreycharles@gmail.com>
2013-09-11 22:43:53 -0400

    Edit linum face

--- init.el
+++ init.el
@@ -80,4 +80,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(linum ((t (:inherit (shadow default) :background "black" :foreground "DarkSlateGray")))))
